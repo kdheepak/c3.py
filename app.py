2528c90b1dfbd8e74d2561eb782bb65c9d46f410
Dheepak Krishnamurthy <kdheepak89@gmail.com>
2015-10-12 23:50:00 -0600

    Add app and template

--- app.py
+++ app.py
@@ -1,12 +1,44 @@
 from flask import Flask
+from flask import render_template
 app = Flask(__name__)
+app.config['DEBUG'] = True
 
 @app.route("/")
 def main():
-    return "Welcome!"
+    return render_template("index.html")
 
-def parse_reflog():
-    pass
+@app.route("/data")
+def data():
+    import json
+    import git
+
+    import networkx as nx
+    tree = nx.DiGraph()
+
+    parent = None
+
+    g = git.Git(repo_path) 
+    repo = git.Repo(repo_path)
+
+    loginfo = g.log()
+
+    def get_hash(commit):
+        return commit.split("\n")[0]
+
+    commits = loginfo.split("\n\ncommit ")
+    commits[0] = commits[0].replace("commit ", '')
+
+    for item in commits[:15]:
+        node = "{}".format(get_hash(item))
+        tree.add_node(node)
+        commit = repo.commit(get_hash(item))
+        for parent in commit.parents:
+            tree.add_edge(node, parent.hexsha)
+
+    from networkx.readwrite import json_graph
+    data = json_graph.node_link_data(tree)
+    j = json.dumps(data)
+    return(j)
 
 if __name__ == "__main__":
     app.run()
